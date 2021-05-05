3704f7d042117fb91a37af69557598d0c86e15bb
Alex Hek <alexanderhek@gmail.com>
2021-05-05 11:57:29 +0100

    finished styling menu-item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,7 +5,7 @@ import './directory.styles.scss';
 
 class Directory extends React.Component {
   constructor(props) {
-    super(props);
+    super(props);  
     this.state = {
       sections: [
         {
@@ -48,9 +48,9 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {
-          this.state.sections.map(({id, title, imageUrl}) => {
-            return <MenuItem id={id} title={title} imageUrl={imageUrl}></MenuItem>
-          })
+          this.state.sections.map(({ id, title, imageUrl, size }) => (
+            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          ))
         }
       </div>
     );
