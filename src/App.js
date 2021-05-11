5357189d9e62ea21686a3a294d264c2895afa7e5
Alex Hek <alexanderhek@gmail.com>
2021-05-11 14:01:15 +0100

    created createUserProfileDocument function that adds userAuth object to our database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -20,8 +20,8 @@ class App extends React.Component {
 
   componentDidMount() {
     // Open auth subscription:
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+      createUserProfileDocument(user);
       console.log(user);
     });
   }
--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
-
 import './form-input.styles.scss';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
   <div className='group'>
     <input className='form-input' onChange={handleChange} {...otherProps} />
-    {
-      label ? (
-        <label 
-          htmlFor={otherProps.id}
-          className={`
+    {label ? (
+      <label
+        htmlFor={otherProps.id}
+        className={`
             ${otherProps.value.length ? 'shrink' : ''} 
-            form-input-label`
-          }
-        >
-          {label}
-        </label>
-      ) : null
-    }
+            form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
   </div>
 );
 
