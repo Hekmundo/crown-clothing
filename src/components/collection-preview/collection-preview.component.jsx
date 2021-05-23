5c51f09ced445c4caa618d2e40cf8b4db2939014
Alex Hek <alexanderhek@gmail.com>
2021-05-23 12:23:39 +0100

    converted collections-overview and collection-preview components to styled components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import './collection-preview.styles.scss';
+import { CollectionPreviewContainer, Title } from './collection-preview.styles';
+import { Items } from '../collection/collection.styles';
 import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className='preview'>
+  <CollectionPreviewContainer>
+    <Title>{title.toUpperCase()}</Title>
+    <Items>
       {items.slice(0, 4).map((item) => (
         <CollectionItem key={item.id} item={item} />
       ))}
-    </div>
-  </div>
+    </Items>
+  </CollectionPreviewContainer>
 );
 
 export default CollectionPreview;
---
+++ src/components/collection-preview/collection-preview.styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const CollectionPreviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  font-size: 28px;
+  margin-bottom: 25px;
+`;
